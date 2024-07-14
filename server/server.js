287f6bbcb1432e4b8a51811d2a112d59954f9947
Fabian Gumbrecht <fabiangumbrecht@web.de>
2024-07-14 19:20:53 +0200

    prepare angular, prepare express server

--- server/server.js
+++ server/server.js
@@ -1,2 +1,21 @@
-var content = "dragonball";
-console.log(content);
+const express = require("express");
+const http = require("http");
+const WebSocket = require("ws");
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({server});
+
+wss.on('connection', (ws, req) => {
+  console.log(req.url);
+});
+
+app.use(express.static(`${__dirname}/dist/browser`))
+
+app.get('/', async (req, res) => {
+  res.status(200).sendFile('index.html');
+});
+
+server.listen(8080, '0.0.0.0', () => {
+  console.log(`Server started on ${JSON.stringify(server.address())}`);
+});
